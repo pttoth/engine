770661b54cdf9dfcd3727ce2c92a809a8a9d387c
Peter Toth <peter.t.toth92@gmail.com>
2018-07-23 08:44:11 +0200

    SDL working

--- engine.pro
+++ engine.pro
@@ -3,4 +3,31 @@ CONFIG -= app_bundle
 CONFIG -= qt
 CONFIG += console c++11
 
+TARGET = Engine
+
+INCLUDEPATH += \
+                "/usr/include/" \
+                "$$PWD/include" \
+               "$$PWD/lib/SDL2-2.0.8/i686-w64-mingw32/include/SDL2/" \
+               "$$PWD/lib/SDL2_image-2.0.3/i686-w64-mingw32/include/SDL2/" \
+#                "$$PWD/lib/SDL2-2.0.8/i686-w64-mingw32/include/SDL2/" \
+#               "$$PWD/lib/SDL2-2.0.8/x86_64-w64-mingw32/include/SDL2/" \
+#                "$$PWD/lib/SDL2_image-2.0.3/x86_64-w64-mingw32/include/SDL2/"
+
+LIBS += -L"/usr/lib" \
+        -L"$$PWD/lib/SDL2-2.0.8/i686-w64-mingw32/lib/" \
+        -L"$$PWD/lib/SDL2_image-2.0.3/i686-w64-mingw32/lib/" \
+#        -L"$$PWD/lib/SDL2-2.0.8/x86_64-w64-mingw32/lib" \
+#        -L"$$PWD/lib/SDL2_image-2.0.3/x86_64-w64-mingw32/lib/" \
+        "-lmingw32" \
+        "-lSDL2main" \
+        "-lSDL2" \
+        "-lSDL2_image"
+        #"-lSDL2_ttf" \
+        #"-lSDL2_mixer"
+
+
+#HEADERS += \
+
 SOURCES += main.cpp
+
--- main.cpp
+++ main.cpp
@@ -1,9 +1,69 @@
-#include <iostream>
+/** -----------------------------------------------------------------------------
+  * FILE:
+  * AUTHOR:
+  * EMAIL:
+  * PURPOSE:
+  * -----------------------------------------------------------------------------
+  */
 
-using namespace std;
+#include <random>
+#include <time.h>
 
-int main()
-{
-    cout << "Hello World!" << endl;
+#include "SDL.h"
+#include "SDL_image.h"
+#include "SDL_events.h"
+
+int main(int argc, char *argv[]){
+
+    SDL_Init(SDL_INIT_VIDEO);
+
+    SDL_Event ev;
+    SDL_Window* window;
+    SDL_Renderer* renderer;
+
+    size_t winx = 32;
+    size_t winy = 32;
+    size_t winw = 640;
+    size_t winh = 480;
+    window = SDL_CreateWindow("anyad",
+                              winx, winy,
+                              winw, winh,
+                              SDL_WINDOW_OPENGL
+                              |SDL_WINDOW_BORDERLESS);
+
+    renderer = SDL_CreateRenderer(window, -1,
+                                  SDL_RENDERER_ACCELERATED
+                                  |SDL_RENDERER_PRESENTVSYNC);
+
+    char red, green, blue;
+    red = 255;
+    green = 255;
+    blue = 255;
+
+    srand(time(NULL));
+
+
+    int counter = 0;
+    bool done = false;
+    while(!done){
+        SDL_PollEvent(&ev);
+        if(ev.type == SDL_MOUSEBUTTONDOWN){
+            done = true;
+        }
+        if(0 == counter % 10){
+            red = rand()*255;
+            green = rand()*255;
+            blue = rand()*255;
+
+            SDL_SetRenderDrawColor(renderer, red, green, blue, 255);
+            SDL_RenderClear(renderer);
+            SDL_RenderPresent(renderer);
+        }
+        ++counter;
+        SDL_Delay(25);
+    }
+
+    SDL_Quit();
+
     return 0;
 }
