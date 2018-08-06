031b29724f8c760620a7a43bf5a8efec6fd379bf
Peter Toth <peter.t.toth92@gmail.com>
2018-08-06 19:35:36 +0200

    merged CopyDLLs_win.bat with SDL_net functionality from addsdlfunctionality branch

--- CopyDLLs_win.bat
+++ CopyDLLs_win.bat
@@ -1,5 +1,11 @@
+::copy SDL 2.0.8. x64
 copy .\lib\SDL2-2.0.8\i686-w64-mingw32\bin\SDL2.dll ..\build-engine-Desktop_Qt_5_10_1_MinGW_32bit-Debug\debug\
 copy .\lib\SDL2-2.0.8\i686-w64-mingw32\bin\SDL2.dll ..\build-engine-Desktop_Qt_5_10_1_MinGW_32bit-Debug\release\
+
+::copy SDL_net 2.0.1 x64
+copy .\lib\SDL2_net-2.0.1-win32-x64\SDL2.dll ..\build-engine-Desktop_Qt_5_10_1_MinGW_32bit-Debug\debug\
+copy .\lib\SDL2_net-2.0.1-win32-x64\SDL2.dll ..\build-engine-Desktop_Qt_5_10_1_MinGW_32bit-Debug\release\
+
 
 pause
 
