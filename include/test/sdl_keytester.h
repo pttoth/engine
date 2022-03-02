da39c134c5b6a030efa940c2decc4cdcc599467a
Peter Toth <peter.t.toth92@gmail.com>
2022-03-02 17:36:09 +0100

    added missing override keywords

--- include/test/sdl_keytester.h
+++ include/test/sdl_keytester.h
@@ -11,23 +11,23 @@ public:
 
     // Game interface
 protected:
-    void onMouseButtonDown(int32_t x, int32_t y, uint8_t button, uint8_t clicks, uint32_t timestamp, uint32_t mouseid);
-    void onMouseButtonUp(int32_t x, int32_t y, uint8_t button, uint8_t clicks, uint32_t timestamp, uint32_t mouseid);
-    void onMouseMotion(int32_t x, int32_t y, int32_t x_rel, int32_t y_rel, uint32_t timestamp, uint32_t mouseid);
-    void onMouseWheel(int32_t x, int32_t y, uint32_t timestamp, uint32_t mouseid, uint32_t direction);
-    void onKeyDown(SDL_Keycode keycode, uint16_t keymod, uint32_t timestamp, uint8_t repeat);
-    void onKeyUp(SDL_Keycode keycode, uint16_t keymod, uint32_t timestamp, uint8_t repeat);
+    void onMouseButtonDown(int32_t x, int32_t y, uint8_t button, uint8_t clicks, uint32_t timestamp, uint32_t mouseid) override;
+    void onMouseButtonUp(int32_t x, int32_t y, uint8_t button, uint8_t clicks, uint32_t timestamp, uint32_t mouseid) override;
+    void onMouseMotion(int32_t x, int32_t y, int32_t x_rel, int32_t y_rel, uint32_t timestamp, uint32_t mouseid) override;
+    void onMouseWheel(int32_t x, int32_t y, uint32_t timestamp, uint32_t mouseid, uint32_t direction) override;
+    void onKeyDown(SDL_Keycode keycode, uint16_t keymod, uint32_t timestamp, uint8_t repeat) override;
+    void onKeyUp(SDL_Keycode keycode, uint16_t keymod, uint32_t timestamp, uint8_t repeat) override;
     void onTouchInputEvent();
 
 
 protected:
-    void onStart();
-    void onExit();
+    void onStart() override;
+    void onExit() override;
     void updateGameState(float t, float dt);
 
     // Game interface
 protected:
-    void tick(float t, float dt)override;
+    void tick(float t, float dt) override;
 };
 
 #endif // SDL_KEYTESTER_H
