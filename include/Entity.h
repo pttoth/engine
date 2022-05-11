7a3e59ae3faafafb5cc8e4eaef978d3eaf796998
pttoth <peter.t.toth92@gmail.com>
2022-05-11 06:58:01 +0200

    minor indent refactor in Entity.h

--- include/Entity.h
+++ include/Entity.h
@@ -51,101 +51,101 @@ public:
 } //end of namespace 'entity'
 
 
-    enum class TickGroup{
-        NO_GROUP = 0, //TODO: probably needs deletion
-        PREPHYSICS,
-        DURINGPHYSICS,
-        POSTPHYSICS,
-    };
+enum class TickGroup{
+    NO_GROUP = 0, //TODO: probably needs deletion
+    PREPHYSICS,
+    DURINGPHYSICS,
+    POSTPHYSICS,
+};
 
-    class Entity{
-        friend class entity::ComponentVisitor;
+class Entity{
+    friend class entity::ComponentVisitor;
 
-    public:
-        const std::string& GetName() const;
-    private:
-        const std::string mName;
+public:
+    const std::string& GetName() const;
+private:
+    const std::string mName;
 
-        BasicPositionComponent          mRootComponent;
-        std::vector<Component*>         mComponents;
-        std::vector<WorldComponent*>    mWorldComponents;
+    BasicPositionComponent          mRootComponent;
+    std::vector<Component*>         mComponents;
+    std::vector<WorldComponent*>    mWorldComponents;
 //private functions
-        int indexOfComponent(Component* const c) const;
-    protected:
-        //virtual void OnCreateContext() = 0;
-        //virtual void OnDeleteContext() = 0;
-    public:
-        /**
-         * @brief RegisterEntity
-         *          Registers the Entity and its Components to the Engine
-         */
-        static void RegisterEntity(Entity* subject);
-        /**
-         * @brief UnregisterEntity
-         *          Unregisters the Entity and its Components from the Engine
-         *          -removes any Tick registrations at the start of the next frame
-         * @param e
-         */
-        static void UnregisterEntity(Entity* subject);
+    int indexOfComponent(Component* const c) const;
+protected:
+    //virtual void OnCreateContext() = 0;
+    //virtual void OnDeleteContext() = 0;
+public:
+    /**
+     * @brief RegisterEntity
+     *          Registers the Entity and its Components to the Engine
+     */
+    static void RegisterEntity(Entity* subject);
+    /**
+     * @brief UnregisterEntity
+     *          Unregisters the Entity and its Components from the Engine
+     *          -removes any Tick registrations at the start of the next frame
+     * @param e
+     */
+    static void UnregisterEntity(Entity* subject);
 
-        virtual void OnRegister() = 0;
-        virtual void OnUnregister() = 0;
+    virtual void OnRegister() = 0;
+    virtual void OnUnregister() = 0;
 
-        virtual void tick(float t, float dt) = 0;
+    virtual void tick(float t, float dt) = 0;
 
-        Entity(const std::string& name);
-        Entity(const Entity& other);
-        Entity(Entity&& other) = delete;
-        virtual ~Entity();
-        Entity& operator=(const Entity &other) = delete;
-        Entity& operator=(Entity &&other) = delete;
-        bool operator==(const Entity &other) const;
+    Entity(const std::string& name);
+    Entity(const Entity& other);
+    Entity(Entity&& other) = delete;
+    virtual ~Entity();
+    Entity& operator=(const Entity &other) = delete;
+    Entity& operator=(Entity &&other) = delete;
+    bool operator==(const Entity &other) const;
 //Entity functions
-        void addComponent(Component* component);
-        void removeComponent(Component* component);
-        std::vector<Component*> getComponents();
-        const WorldComponent* getRootComponent() const;
-        WorldComponent* getRootComponent();
+    void addComponent(Component* component);
+    void removeComponent(Component* component);
+    std::vector<Component*> getComponents();
+    const WorldComponent* getRootComponent() const;
+    WorldComponent* getRootComponent();
 
-        void Spawn();
-        void Despawn();
+    void Spawn();
+    void Despawn();
 
-        //virtual void CreateContext();
-        //virtual void DeleteContext();
+    //virtual void CreateContext();
+    //virtual void DeleteContext();
 
 //tick
-    private:
-        bool        mTickEnabled;
-        TickGroup   mTickGroup;
-        float       mTickInterval;
-        float       mTickLast;
-        bool        mTickRegistered;
-        bool        mRegistered;
+private:
+    bool        mTickEnabled;
+    TickGroup   mTickGroup;
+    float       mTickInterval;
+    float       mTickLast;
+    bool        mTickRegistered;
+    bool        mRegistered;
 
-        void AddWorldComponent(WorldComponent* component);
-        void RemoveWorldComponent(WorldComponent* component);
-    public:
-        static void RegisterTickFunction(Entity* subject, TickGroup group = TickGroup::DURINGPHYSICS);
-        static void UnregisterTickFunction(Entity* subject);
-        static void AddTickDependency(Entity* subject, Entity* dependency);
-        static void RemoveTickDependency(Entity* subject, Entity* dependency);
-        void registerComponents();
-        bool isRegistered() const;
-        bool isTickRegistered() const;
-        void enableTick();
-        void disableTick();
-        bool isEnabled() const;
-        void tickEntity(float t, float dt);
+    void AddWorldComponent(WorldComponent* component);
+    void RemoveWorldComponent(WorldComponent* component);
+public:
+    static void RegisterTickFunction(Entity* subject, TickGroup group = TickGroup::DURINGPHYSICS);
+    static void UnregisterTickFunction(Entity* subject);
+    static void AddTickDependency(Entity* subject, Entity* dependency);
+    static void RemoveTickDependency(Entity* subject, Entity* dependency);
+    void registerComponents();
+    bool isRegistered() const;
+    bool isTickRegistered() const;
+    void enableTick();
+    void disableTick();
+    bool isEnabled() const;
+    void tickEntity(float t, float dt);
 
-        /**
-         * @brief setTickInterval
-         * @param interval: unit (ms)
-         * @throws 'std::out_of_range' on negative values
-         */
-        void setTickInterval(float interval);
-        float getTickInterval() const;
+    /**
+     * @brief setTickInterval
+     * @param interval: unit (ms)
+     * @throws 'std::out_of_range' on negative values
+     */
+    void setTickInterval(float interval);
+    float getTickInterval() const;
 
-        TickGroup getTickGroup() const;
-    };
+    TickGroup getTickGroup() const;
+};
 
 }
