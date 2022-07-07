6ab97e930a36aa8efa67ef39924a85792b7f0953
Peter Toth <peter.t.toth92@gmail.com>
2022-07-07 17:20:08 +0200

    OpenGL Tester 01   added shader sources   now uses ShaderProgram class   switched some OpenGL calls with wrapped versions

--- src/engine/test/ogltester01/Game.cpp
+++ src/engine/test/ogltester01/Game.cpp
@@ -7,6 +7,8 @@
 
 
 #include "pt/logging.h"
+#include "GlWrapper.h"
+#include "ShaderProgram.h"
 
 #include <algorithm>
 #include <sstream>
@@ -18,8 +20,94 @@ using namespace test;
 using namespace test::ogltester01;
 
 using namespace engine;
+using namespace pt;
 using namespace pt::math;
 
+
+const char* VertexShader = R"(
+    #version 330
+    precision highp float;
+
+    uniform float       Scale;
+    uniform mat4        M;
+    uniform mat4        VP;
+    uniform mat4        MVP;
+    uniform int         UseFixColor;
+    uniform vec3        Color;
+
+    uniform vec3        LightAmbient;
+
+    layout(location = 0) in vec3 in_vPos;
+    layout(location = 1) in vec2 in_tPos;
+    layout(location = 2) in vec3 in_Normal;
+
+    out     vec3    vPos;
+    out     vec2    tPos;
+    out     vec3    Normal;
+
+    void main(){
+        gl_Position = vec4(in_vPos, 1.0f) * MVP;
+        if(0 == UseFixColor){
+            tPos    = in_tPos;
+        }else{
+            tPos    = in_tPos; //don't care
+        }
+        //Normal = (vec4(in_Normal, 0.0f) * M).xyz;
+        Normal = in_Normal;
+    }
+)";
+
+
+const char* GeometryShader = R"()";
+const char* FragmentShader = R"(
+    #version 330
+    precision highp float;
+
+    struct Spotlight{
+        bool    enabled;
+        vec3    color;
+        float   intensity_diffuse;
+        vec3    direction;
+    };
+
+    uniform sampler2D       gSampler;
+    uniform int             UseFixColor;
+    uniform vec3            Color;
+    uniform int             UseAlphaOverride;
+    uniform float           Alpha;
+
+    uniform vec3            LightAmbient;
+    uniform Spotlight       Spotlights[64];
+
+
+    in      vec3    vPos;
+    in      vec2    tPos;
+    out     vec3    Normal;
+    out     vec4    FragColor;
+
+    float GetAlpha(){
+        if(0 == UseAlphaOverride){  return 1.0f;
+        }else{                      return Alpha;
+        }
+    }
+
+    void main(){
+        float a = GetAlpha();
+        vec4 retval = vec4(1.0f,1.0f,1.0f,1.0f);
+
+        for(int i=0; i<64;++i){
+            if(Spotlights[i].enabled){
+                //calculate spotlight effects
+            }
+        }
+
+        if(0 == UseFixColor){   retval = texture(gSampler, tPos);
+        }else{                  retval = vec4(Color, a);
+        }
+
+        FragColor = vec4( retval.xyz * LightAmbient, retval.w);
+    }
+)";
 
 Game::
 Game()
@@ -188,55 +276,47 @@ InitContext()
 
     pt::log::out << "-----\n";
     int majorVersion, minorVersion;
-    glGetIntegerv(GL_MAJOR_VERSION, &majorVersion);
-    glGetIntegerv(GL_MINOR_VERSION, &minorVersion);
+    gl::GetIntegerv(GL_MAJOR_VERSION, &majorVersion);
+    gl::GetIntegerv(GL_MINOR_VERSION, &minorVersion);
 
-    pt::log::out << "GL Vendor    : " << glGetString(GL_VENDOR) << "\n";
-    pt::log::out << "GL Renderer  : " << glGetString(GL_RENDERER) << "\n";
-    pt::log::out << "GL Version (string)  : " << glGetString(GL_VERSION) << "\n";
+    pt::log::out << "GL Vendor    : " << gl::GetString(GL_VENDOR) << "\n";
+    pt::log::out << "GL Renderer  : " << gl::GetString(GL_RENDERER) << "\n";
+    pt::log::out << "GL Version (string)  : " << gl::GetString(GL_VERSION) << "\n";
     pt::log::out << "GL Version (integer) : " << majorVersion << "." << minorVersion << "\n";
-    pt::log::out << "GLSL Version : " << glGetString(GL_SHADING_LANGUAGE_VERSION) << "\n";
+    pt::log::out << "GLSL Version : " << gl::GetString(GL_SHADING_LANGUAGE_VERSION) << "\n";
 
     pt::log::out << "-----\ncompiling shaders...";
 
-    assert(false);
-    int failed = true;
-    //int failed = CompileShaders();
-    if( failed ){
-        SDL_DestroyWindow( mWindow );
-
-        std::stringstream ss;
-        ss << "failed!";
-        pt::log::out << ss.str() << "\n";
-        throw std::runtime_error( "Failed to compile shaders" );
+    try{
+        ShaderProgram shaderprogram( std::string(VertexShader),
+                                     std::string(GeometryShader),
+                                     std::string(FragmentShader));
+    }catch( const std::exception& e ){
+        pt::log::err << "Couldn't create shaderprogram!\n";
+        //TODO: free allocated stuff
+        throw;
     }
 
     //enable depth testing
-    glEnable(GL_DEPTH_TEST);
-
+    gl::Enable(GL_DEPTH_TEST);
 
     //enable backface culling
-    glFrontFace(GL_CW);
-    glCullFace(GL_BACK);
-    glEnable(GL_CULL_FACE);
-    glEnable (GL_BLEND);
-    glBlendFunc (GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+    gl::FrontFace(GL_CW);
+    gl::CullFace(GL_BACK);
+    gl::Enable(GL_CULL_FACE);
+    gl::Enable (GL_BLEND);
+    gl::BlendFunc (GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
     //draw something
-    glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-    SDL_GL_SwapWindow( mWindow );
+    gl::ClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+    gl::Clear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    SDL_GL_SwapWindow( mWindow ); //TODO: may need to be moved under a mutex
     pt::log::out << " done\n";
 
 
-    assert(false); //TODO: move the GL calls into a separate handler class
-
-
-    assert(false); //TODO: fix the code order below (may need to move up)
+    //assert(false); //TODO: fix the code order below (may need to move up)
 
     sdl->SetMainWindow(mWindow);
-
-
     sdl->SetMainWindowWidth(pxWidth);
     sdl->SetMainWindowHeight(pxHeight);
 
