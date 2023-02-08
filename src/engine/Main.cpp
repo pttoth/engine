6a282d79ba37a41c681aa8bd6814270cc047816d
Peter Toth <peter.t.toth92@gmail.com>
2023-02-09 00:04:35 +0100

    added stacktrace printer function the testing program will now print stacktraces on failed assertions and segmentation faults

--- src/engine/Main.cpp
+++ src/engine/Main.cpp
@@ -12,9 +12,36 @@
 #include <exception>
 
 
+#include <stdio.h>
+#include <execinfo.h>
+#include <signal.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+
+// callback to call when encountering specific process signal
+void handler(int sig)
+{
+    size_t  size;
+    const size_t  maxsize = 256;
+    void*   array[maxsize];
+
+    // get void*'s for all entries on the stack
+    size = backtrace( array, maxsize );
+
+    // print out all the frames to stderr
+    fprintf(stderr, "Error: signal %d:\n", sig);
+    backtrace_symbols_fd(array, size, STDERR_FILENO);
+    exit(1);
+}
+
+
 int
 main(int argc, char *argv[])
 {
+    signal(SIGSEGV, handler);   // install our handler
+    signal(SIGABRT, handler);   // install our handler
+
     bool done = false;
 
     enum eSettings{
