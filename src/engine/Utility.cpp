cb15edd8f3b2c4f96c95bf8bd115bf166f7b1bfb
Peter Toth <peter.t.toth92@gmail.com>
2024-03-22 02:56:06 +0100

    moved 'Utility::CalcRotMtx()' to 'CameraPerspective::GetRotationMtx()'

--- src/engine/Utility.cpp
+++ src/engine/Utility.cpp
@@ -19,44 +19,11 @@ CalcMVP( const engine::WorldComponent& component, const engine::Camera& camera )
 }
 
 
-//TODO: remove
 math::float4x4 engine::
 CalcRotMtx( const math::float3& direction, const math::float3& preferredUp )
 {
-    PT_LOG_ONCE_WARN( "Reminder: 'Utility::CalcRotMtx()' has to be reviewed/reimplemented!" );
-    static bool firsttime = true;
-    if( firsttime ){
-        PT_WARN_UNIMPLEMENTED_FUNCTION // TEMP: display stacktrace of usages
-        firsttime = false;
-    }
-
-    assert( 0 != direction.length() );
-    if( 0 == direction.length() ){
-        PT_LOG_ERR( "CalcRotMtx: null-vector supplied as 'direction!'" );
-        return math::float4x4::identity;
-    }
-    math::float3 dir = direction.normalize();
-    math::float3 right = dir.cross( preferredUp.normalize() );
-
-    bool gimbal_lock = !(0.0f < right.length());
-    if( gimbal_lock ){
-        PT_LOG_ERR( "CalcRotMtx: Gimbal lock occured!" );
-        PT_LOG_ERR( "dir: \n" << ToString(dir) );
-        PT_LOG_ERR( "preferredUp: \n" << ToString(preferredUp) );
-        assert( !gimbal_lock );
-        return math::float4x4::identity;
-    }
-    math::float3 up = right.cross( dir );
-    math::float4x4  orient = math::float4x4::identity;
-
-    //(World)X stays    X(Screen)
-    //(World)Y becomes -Z(Screen)
-    //(World)Z becomes  Y(Screen)
-    orient.m[0][0] = right.v[0];    orient.m[0][1] = right.v[1];    orient.m[0][2] = right.v[2];
-    orient.m[1][0] = up.v[0];       orient.m[1][1] = up.v[1];       orient.m[1][2] = up.v[2];
-    orient.m[2][0] = -dir.v[0];     orient.m[2][1] = -dir.v[1];     orient.m[2][2] = -dir.v[2];
-
-    return orient;
+    PT_UNIMPLEMENTED_FUNCTION
+    return math::float4x4();
 }
 
 
--- src/engine/actor/CameraPerspective.cpp
+++ src/engine/actor/CameraPerspective.cpp
@@ -70,7 +70,36 @@ LookAt( const float3& lookat_pos )
 math::float4x4 CameraPerspective::
 GetRotationMtx() const
 {
-    return CalcRotMtx( mLookatRelative, mPreferredUp );
+    const math::float3& direction = mLookatRelative;
+    const math::float3& preferredUp = mPreferredUp;
+
+    if( 0 == direction.length() ){
+        PT_LOG_ERR( "Camera '" << GetName() << "': null-vector supplied as 'direction!'" );
+        assert( 0 != direction.length() );
+        return math::float4x4::identity;
+    }
+    math::float3 dir = direction.normalize();
+    math::float3 right = dir.cross( preferredUp.normalize() );
+
+    bool gimbal_lock = !(0.0f < right.length());
+    if( gimbal_lock ){
+        PT_LOG_ERR( "CalcRotMtx: Gimbal lock occured!" );
+        PT_LOG_ERR( "dir: \n" << ToString(dir) );
+        PT_LOG_ERR( "preferredUp: \n" << ToString(preferredUp) );
+        assert( !gimbal_lock );
+        return math::float4x4::identity;
+    }
+    math::float3    up      = right.cross( dir );
+    math::float4x4  orient  = math::float4x4::identity;
+
+    //(World)X stays    X(Screen)
+    //(World)Y becomes -Z(Screen)
+    //(World)Z becomes  Y(Screen)
+    orient.m[0][0] = right.v[0];    orient.m[0][1] = right.v[1];    orient.m[0][2] = right.v[2];
+    orient.m[1][0] = up.v[0];       orient.m[1][1] = up.v[1];       orient.m[1][2] = up.v[2];
+    orient.m[2][0] = -dir.v[0];     orient.m[2][1] = -dir.v[1];     orient.m[2][2] = -dir.v[2];
+
+    return orient;
 }
 
 
