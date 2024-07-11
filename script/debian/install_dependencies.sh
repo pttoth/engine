3fd53169fddb01b3327e0f36bc762fc4a1b0404d
Peter Toth <peter.t.toth92@gmail.com>
2024-07-12 00:22:23 +0200

    updated debian dependency install script to include 'libasio-dev'

--- script/debian/install_dependencies.sh
+++ script/debian/install_dependencies.sh
@@ -8,6 +8,7 @@ packages="$packages libglew-dev"
 packages="$packages libsdl2-dev"
 packages="$packages libpng-dev"
 packages="$packages libassimp-dev"
+packages="$packages libasio-dev"
 
 echo "installing packages:"
 echo "  $packages"
