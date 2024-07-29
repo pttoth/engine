97069a3c02f5ad96f586cc89ad59e0099a484107
pttoth <peter.t.toth92@gmail.com>
2024-07-30 01:33:58 +0200

    main.cpp   updated signal handler to use 'PT_PRINT_STACKTRACE()'

--- src/test/opengl_test/main.cpp
+++ src/test/opengl_test/main.cpp
@@ -2,6 +2,7 @@
 #include "test/opengl_test/Game.h"
 #include "test/opengl_test/Pawn.h"
 
+#include "engine/Utility.h"
 #include "pt/def.h"
 
 #include <exception>
@@ -22,22 +23,7 @@
 void
 Handler(int sig )
 {
-#ifdef PT_PLATFORM_LINUX
-    size_t  size;
-    const size_t  maxsize = 256;
-    void*   array[maxsize];
-
-    // get void*'s for all entries on the stack
-    size = backtrace( array, maxsize );
-
-    // print out all the frames to stderr
-    fprintf(stderr, "---------- Stack trace ----------\n" );
-    fprintf(stderr, "Error: signal %d:\n", sig);
-    backtrace_symbols_fd(array, size, STDERR_FILENO);
-    fprintf(stderr, "\n");
-#else
-    PT_UNIMPLEMENTED_FUNCTION
-#endif
+    PT_PRINT_STACKTRACE();
     exit(1);
 }
 
