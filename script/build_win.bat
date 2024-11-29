8da840792d60162ffa5e73a730c5788ec032188a
Peter Toth <peter.t.toth92@gmail.com>
2024-11-29 09:07:37 +0100

    windows build script   added debug version ('win64_debug')   now uses 'mingw32-make' instead of 'make'   minor readability improvements

---
+++ script/build_win.bat
@@ -0,0 +1,35 @@
+@echo off
+pushd %~dp0
+
+call .\win\set_env.bat
+::gets the following variables
+::  win_script_dir
+::  root_directory
+::  build_platform
+::  temp_dir
+@echo off
+
+set build_directory=%root_directory%\build\%build_platform%
+
+pushd "%root_directory%"
+mkdir "%build_directory%"
+
+cd "%root_directory%\projects\%build_platform%\"
+cmake . -B"%build_directory%" -G"MinGW Makefiles" -DCMAKE_BUILD_TYPE=Release
+
+@echo ------------------------------
+@echo building with %NUMBER_OF_PROCESSORS% threads
+@echo ------------------------------
+pushd "%build_directory%"
+mingw32-make -j %NUMBER_OF_PROCESSORS% --output-sync
+:: return from build dir
+popd
+
+
+
+:: return from project rootdir
+popd
+
+:: return from script dir
+popd
+@echo on
--- script/build_win_debug.bat
+++ script/build_win_debug.bat
@@ -9,24 +9,27 @@ call .\win\set_env.bat
 ::  temp_dir
 @echo off
 
-set build_directory=%root_directory%\build
+set build_directory=%root_directory%\build\%build_platform%_debug
 
 pushd "%root_directory%"
-
-mkdir "%root_directory%\build"
+mkdir "%build_directory%"
 
 cd "%root_directory%\projects\%build_platform%\"
-
-cmake . -B"%build_directory%" -G"MinGW Makefiles" -DCMAKE_BUILD_TYPE=Release
-
-cd "%build_directory%"
+cmake . -B"%build_directory%" -G"MinGW Makefiles" -DCMAKE_BUILD_TYPE=Debug
 
 @echo ------------------------------
 @echo building with %NUMBER_OF_PROCESSORS% threads
 @echo ------------------------------
-make -j %NUMBER_OF_PROCESSORS% --output-sync
-
+pushd "%build_directory%"
+mingw32-make -j %NUMBER_OF_PROCESSORS% --output-sync
+:: return from build dir
 popd
 
+
+
+:: return from project rootdir
+popd
+
+:: return from script dir
 popd
 @echo on
