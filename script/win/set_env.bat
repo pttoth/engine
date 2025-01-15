47fd5734ab99b95f43e74aab1a6b5bdac119ecca
Peter Toth <peter.t.toth92@gmail.com>
2025-01-15 21:34:48 +0100

    minor fix to 'script/win/set_env.bat'

--- script/win/set_env.bat
+++ script/win/set_env.bat
@@ -5,6 +5,7 @@ pushd %~dp0
 set win_script_dir=%~dp0
 set root_directory=%win_script_dir%\..\..
 
+::--------------------------------------------------
 :: NOTE: the following files are both present in MinGW-8.1.0 and Git-s own MinGW environment
 ::  libgcc_s_seh-1.dll
 ::  libssp-0.dll
@@ -16,7 +17,16 @@ set root_directory=%win_script_dir%\..\..
 
 ::  Git's MinGW environment is currently only used by the project to access 'curl'
 ::  There probably won't be any more conflicts as the windows build env is moving to a cross-build Docker container soon
-set PATH="C:\tools\mingw\x86_64-8.1.0-posix-seh\mingw64\bin;C:\Program Files\Git\bin;C:\tools\CMake\bin;C:\Program Files\7-Zip;%PATH%"
+::--------------------------------------------------
+
+if not defined pt_path_set (
+    set pt_path_set=0
+)
+
+if not "%pt_path_set%"=="1" (
+    set PATH="C:\tools\mingw\x86_64-8.1.0-posix-seh\mingw64\bin;C:\Program Files\Git\bin;C:\tools\CMake\bin;C:\Program Files\7-Zip;%PATH%"
+    set pt_path_set=1
+)
 
 set build_platform=win64
 
